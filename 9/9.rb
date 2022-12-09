bf29228f2582eda40b1dd3888a263739a4cb4c71
Jim Challenger <jimchallenger1@gmail.com>
2022-12-09 13:53:08 -0600

    Better 9

--- 9/9.rb
+++ 9/9.rb
@@ -1,62 +1,42 @@
 class Day9
   def initialize(file)
     @inputs = parse_file(file)
-    @visited = Set.new([[0,0]])
   end
 
   def part_one
-    @knots = {0 => [0,0], 1 => [0,0]}
-    find_num_tail_visits
+    find_num_tail_visits(1)
   end
 
   def part_two
-    @knots = Hash[((0..9).to_a).collect {|el| [el, [0,0]]}]
-    find_num_tail_visits
+    find_num_tail_visits(9)
   end
 
-  def move_tail(h,t)
-    hx,hy = @knots[h]
-    tx,ty = @knots[t]
-    return if (hx-tx).abs <=1 && (hy-ty).abs <= 1
+  def find_num_tail_visits(tail_count)
+    visited = Set.new([[0,0]])
+    knots = Hash[((0..tail_count).to_a).collect {|el| [el, [0,0]]}]
 
-    nx,ny = if hx-tx == 2 && hy-ty == 0
-      [1,0]
-    elsif hx-tx == -2 && hy-ty == 0
-      [-1,0]
-    elsif hx-tx == 0 && hy-ty == 2
-      [0,1]
-    elsif hx-tx == 0 && hy-ty == -2
-      [0,-1]
-    elsif hx-tx > 0 && hy-ty > 0
-      [1,1]
-    elsif hx-tx > 0 && hy-ty < 0
-      [1,-1]
-    elsif hx-tx < 0 && hy-ty < 0
-      [-1,-1]
-    elsif hx-tx < 0 && hy-ty > 0
-      [-1,1]
-    else
-      [0,0]
-    end
-
-    @knots[t] = [tx+nx,ty+ny]
-  end
-
-  def find_num_tail_visits
     @inputs.each do |dir, n|
       mx,my = {"R" => [1,0], "L" => [-1,0], "U" => [0,1], "D" => [0,-1]}[dir]
 
       n.to_i.times do 
-        x,y = @knots[0]
-        @knots[0] = [x+mx,y+my]
-        @knots.keys[1..-1].each do |n|
-          move_tail(n-1, n)
-          @visited.add(@knots[n]) if n == @knots.keys.last
+        knots[0] = [knots[0][0]+mx,knots[0][1]+my]
+        knots.keys[1..-1].each do |n|
+          new_tail_loc = move_tail(knots[n-1], knots[n])
+          knots[n] = new_tail_loc unless new_tail_loc.nil?
+          visited.add(knots[n]) if n == knots.keys.last
         end
       end
     end
 
-    @visited.count
+    visited.count
+  end
+
+  def move_tail((hx,hy),(tx,ty))
+    return if (hx-tx).abs <= 1 && (hy-ty).abs <= 1
+
+    nx = (hx-tx).clamp(-1,1)
+    ny = (hy-ty).clamp(-1,1)
+    [tx+nx,ty+ny]
   end
 
   def parse_file(file)
